7dd4feddd9d88a9d933b36b78e3b0276d5b4b251
Sid <sidtheminerofficial@gmail.com>
2025-03-19 15:03:05 +0530

    First COmmit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import HeroSection from './heroSection'
+import React from 'react';
+import './index.css';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  )
+}
+
+export default App
